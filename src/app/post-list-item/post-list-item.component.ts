b40b201465a24fae38dbbb5ef08cba0e82ee3191
unknown <sokobachristian@yahoo.fr>
2019-03-17 19:47:50 +0100

    progressing..

--- src/app/post-list-item/post-list-item.component.ts
+++ src/app/post-list-item/post-list-item.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit, Input } from '@angular/core';
 export class PostListItemComponent implements OnInit {
 
   constructor() { }
+  lastUpdate = new Date();
 
   @Input() title: string;
   @Input() content: string;
